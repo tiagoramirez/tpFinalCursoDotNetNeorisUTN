92a123d65dcdecfe245992ee8e0f269a851c5a6d
Tiago Ramirez <tirama2001@gmail.com>
2021-12-06 16:50:20 -0300

    Cambiado menu principal

---
+++ TP_Final/Menus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Final
+{
+    internal class Menus
+    {
+        public static void MostrarMenuPrincipal()
+        {
+            Console.WriteLine("1: Crear nuevo cliente");
+            Console.WriteLine("2: Listar facturas de un cliente y el total de sus ventas");
+            Console.WriteLine("3: Inactivar un paquete");
+            Console.WriteLine("4: Actualizar precio de un paquete");
+            Console.WriteLine("5: Listar clientes que tuvieron al menos 2 ventas");
+            Console.WriteLine("6: Terminar programa");
+        }
+    }
+}
--- TP_Final/Program.cs
+++ TP_Final/Program.cs
@@ -9,8 +9,37 @@ namespace TP_Final
     {
         static void Main(string[] args)
         {
+            var terminar = false;
+            while (!terminar)
+            {
+                Menus.MostrarMenuPrincipal();
+                var opc = Console.ReadKey(true);
+                while (opc.KeyChar != '1' && opc.KeyChar != '2' && opc.KeyChar != '3' && opc.KeyChar != '4' && opc.KeyChar != '5' && opc.KeyChar != '6')
+                {
+                    opc = Console.ReadKey(true);
+                }
+                Console.Clear();
 
-            Cliente c = new Cliente();
+                switch (opc.KeyChar)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        break;
+                    case '6':terminar = true;
+                        break;
+                }
+            }
+            
+            //Esto por el momento lo vamos a inutilizar y a la larga agregarlo en alguna parte de la creacion del cliente!
+
+            /*Cliente c = new Cliente();
             ConsoleKeyInfo continuar;
             string prov;
 
@@ -31,7 +60,7 @@ namespace TP_Final
                 Console.WriteLine("Continuar? (s/n)");
                 continuar = Console.ReadKey(true);
             } while (continuar.KeyChar != 'n' && continuar.KeyChar != 'N');
-            Console.WriteLine(c.Provincia);
+            Console.WriteLine(c.Provincia);*/
         }
     }
 }
