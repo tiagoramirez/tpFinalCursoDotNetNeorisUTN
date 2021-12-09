dc99b3c08aa1345059c76dfd30a2b2664010a48f
Jefferson <jeffersonperezinfo@gmail.com>
2021-12-08 21:38:39 -0300

    cambio factura paquete

--- Dominio/Factura.cs
+++ Dominio/Factura.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,26 @@ namespace Dominio
 {
     public class Factura
     {
+        [Key]
+        public int idFactura { set; get; }
         private int idCliente;
         private double importeTotal;
         private Paquete paquete;
+        private DateTime fechaEmisionFactura;
+
+        public Factura(int idCliente, double importeTotal, Paquete paquete, DateTime fechaEmisionFactura)
+        {
+            this.idCliente = idCliente;
+            this.importeTotal = importeTotal;
+            this.paquete = paquete;
+            this.fechaEmisionFactura = fechaEmisionFactura;
+        }
+
+        public int IdCliente { get => idCliente; set => idCliente = value; }
+        public double ImporteTotal { get => importeTotal; set => importeTotal = value; }
+        public Paquete Paquete { get => paquete; set => paquete = value; }
+        public DateTime FechaEmisionFactura { get => fechaEmisionFactura; set => fechaEmisionFactura = value; }
+       
+
     }
 }
--- Dominio/Paquete.cs
+++ Dominio/Paquete.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Dominio
 {
     public abstract class Paquete
     {
+        [Key]
+        public  int idPaquete { get   ; set ; }
         private string nombre;
         private double precioBase;
         private Lugar[] listaLugares = new Lugar[10];
@@ -21,20 +24,28 @@ namespace Dominio
         public int CantidadDiasTotales { get => cantidadDiasTotales; set => cantidadDiasTotales = value; }
         public DateTime FechaDeViaje { get => fechaDeViaje; set => fechaDeViaje = value; }
         public bool Vigente { get => vigente; set => vigente = value; }
-        //public int CantidadDeCuotas { get => cantidadDeCuotas; set => cantidadDeCuotas = value; }
-        //public double ValorPorCuota { get => valorPorCuota; set => valorPorCuota = value; }
+         
         public  Lugar[] ListaLugares { get => listaLugares; set => listaLugares = value; }
         protected abstract bool cargarPago(int cantidadDeCuotas, double valorPorCuota);
         public Paquete(string nombre, double precioBase, Lugar[] listaLugares, int cantidadDiasTotales, DateTime fechaDeViaje, bool vigente )
         {
-            this.nombre = nombre;
-            this.precioBase = precioBase;
-            this.listaLugares = listaLugares;
+            Console.Write("Ingresar el nombre del paquete: ");
+            this.nombre = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Ingresar el precio base del paquete: ");
+            Double.TryParse(Console.ReadLine(), out this.precioBase);  
+            Console.Clear();
+            
+            Console.Write("Ingrese los lugares que va a visitar  ");
             this.cantidadDiasTotales = cantidadDiasTotales;
             this.fechaDeViaje = fechaDeViaje;
             this.vigente = vigente;
-          //  this.cantidadDeCuotas = cantidadDeCuotas;
-           // this.valorPorCuota = valorPorCuota;
+         
+        }
+        public void crearPaquete()
+        {
+
         }
 
     }
